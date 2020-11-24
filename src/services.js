ca1161c335e8e5f38c30d76de9478e5668b03c03
Carlos Morais <carlos.morais@fastalelo.onmicrosoft.com>
2020-11-24 14:51:42 -0300

    Endpoints

--- src/services.js
+++ src/services.js
@@ -5,7 +5,7 @@ const getManchetes = axios.create({
 })
 
 const savePerfil = axios.create({
-    baseURL: 'https://59mj4amdhi.execute-api.us-east-1.amazonaws.com/quizz/perfil'
+    baseURL: 'http://ec2-54-209-43-99.compute-1.amazonaws.com:8080/quizz/salvar/perfil'
 })
 
 export default { getManchetes, savePerfil };
